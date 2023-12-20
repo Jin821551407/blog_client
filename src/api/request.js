e16dadf82d75b010e63043d84952bf5407eecb1c
lijingyu <18683257353@163.com>
2023-12-20 10:56:15 +0800

    1

--- src/api/request.js
+++ src/api/request.js
@@ -4,6 +4,12 @@ import showMessage from "@/utils/showMessage";
 //创建axios实例
 const ins = axios.create();
 //添加请求拦截器
+ins.interceptors.request.use(function (config) {
+  console.log(config, 'config')
+  config.url = 'http://39.105.222.196:3001' + config.url
+  config.headers['Access-Control-Allow-Origin'] = 'http://39.105.222.196:3001'
+  return config
+})
 
 //添加响应拦截器
 ins.interceptors.response.use(function (res) {
--- vue.config.js
+++ vue.config.js
@@ -11,4 +11,5 @@ module.exports = {
     },
   },
   configureWebpack: require("./webpack.config"),
+  publicPath: './'
 };
