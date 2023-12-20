8934054a83feddeccd5ea99730e09fbb73dc7dab
lijingyu <18683257353@163.com>
2023-12-20 11:55:39 +0800

    Updates

--- src/mock/about.js
+++
@@ -1,7 +0,0 @@
-import Mock from "mockjs";
-
-Mock.mock("/api/About", "get", {
-  code: 0,
-  msg: "",
-  data: "https://www.w3school.com.cn",
-});
--- src/utils/index.js
+++
@@ -1,9 +0,0 @@
-export { default as showMessage } from "./showMessage";
-
-export { default as getComponentRootDom } from "./getComponentRootDom";
-
-export { default as formatDate } from "./formatDate";
-
-export { default as deBounce } from "./deBounce";
-
-export { default as titleController } from "./titleController";
